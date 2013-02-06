8ce1ec7867b0b8b5d9e390307722e6c23aaf7813
Christian Johansen <christian@cjohansen.no>
2013-02-06 09:26:40 +0100

    Add event delegation, Cull-style

--- test/event-test.js
+++ test/event-test.js
@@ -19,5 +19,51 @@ buster.testCase("DOM events", {
         click(el);
 
         assert.calledOnce(handler);
+    },
+
+    "delegation": {
+        setUp: function () {
+            this.el = dome.el("div", [dome.el("a")]);
+        },
+
+        "calls delegate with target element": function () {
+            var delegator = this.spy();
+            dome.delegate("click", delegator, function () {}, this.el);
+
+            click(this.el.firstChild);
+
+            assert.calledOnce(delegator);
+            assert.calledWith(delegator, this.el.firstChild, "click");
+        },
+
+        "does not call handler if delegator returns falsy": function () {
+            var delegator = this.stub().returns(false);
+            var handler = this.spy();
+            dome.delegate("click", delegator, handler, this.el);
+
+            click(this.el.firstChild);
+
+            refute.called(handler);
+        },
+
+        "calls handler if delegator returns truthy": function () {
+            var delegator = this.stub().returns(true);
+            var handler = this.spy();
+            dome.delegate("click", delegator, handler, this.el);
+
+            click(this.el.firstChild);
+
+            assert.calledOnce(handler);
+        },
+
+        "calls handler with target as this": function () {
+            var delegator = this.stub().returns(true);
+            var handler = this.spy();
+            dome.delegate("click", delegator, handler, this.el);
+
+            click(this.el.firstChild);
+
+            assert.calledOn(handler, this.el.firstChild);
+        }
     }
 });
